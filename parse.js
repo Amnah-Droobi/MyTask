4c2108b0ac6edff84f9b0db8139e682b3d22364b
Amnah Droobi <amnahdroobi@hotmail.com>
2018-02-27 14:14:47 -0800

    updated file

--- parse.js
+++ parse.js
@@ -4,97 +4,101 @@ main();
 
 
 function main() {
-  let content = fs.readFileSync('./data.csv', 'utf-8');
-  let { lines, headers, keys } = parseFileToLinesAndHeaders(content);
-  let users = parseLinesInToUsers(lines, keys);
-  //console.log(JSON.stringify(users));
-  let {validUsers,inValidUsers} = validateUsers(users);
-  console.log(JSON.stringify(inValidUsers));
-  //console.log(validuser) ;
-  // inValidUsers);
-  //writeUsersToFile(users);
-  // deletedItemFromArray(users, pos, No);
+    let content = fs.readFileSync('./data.csv', 'utf-8');
+    let { lines, headers, keys } = parseFileToLinesAndHeaders(content);
+    let users = parseLinesInToUsers(lines, keys);
+    //console.log(JSON.stringify(users));
+    let { validUsers, inValidUsers } = validateUsers(users);
+    //console.log(JSON.stringify(inValidUsers));
+    //console.log(validuser) ;
+    // inValidUsers);
+    writeUsersToFile(users);
+    printValidUsersToFile(validUsers);
+    // deletedItemFromArray(users, pos, No);
 }
 
 function parseFileToLinesAndHeaders(content) {
-  let lines = content.split("\n");
-  let headers = lines.shift();
-  let keys = headers.split(",");
-  return { lines, headers, keys };
+    let lines = content.split("\n");
+    let headers = lines.shift();
+    let keys = headers.split(",");
+    return { lines, headers, keys };
 }
 function parseLinesInToUsers(lines, keys) {
-     let value = [];
-   let users = lines.map(line => {
-      value = line.split(",");
-      let obj = {};
-      keys.forEach((item, index) => {
-          obj[item] = value[index];
-      });
-      return obj;
+    let value = [];
+    let users = lines.map(line => {
+        value = line.split(",");
+        let obj = {};
+        keys.forEach((item, index) => {
+            obj[item] = value[index];
+        });
+        return obj;
 
-  });
-  return users;
+    });
+    return users;
 
 }
 
 
 function validateUsers(users) {
-  let validUsers = [];
-  let inValidUsers = [];
-  users.forEach(user => {
-      if (IsValid(user,users)){
+    let validUsers = [];
+    let inValidUsers = [];
+    users.forEach(user => {
+        if (IsValid(user, users)) {
 
-          validUsers.push(user);
-      }
-      else {
-          inValidUsers.push(user);
-      }
-  });
-  return{
-       validUsers,
-   inValidUsers
-   }
+            validUsers.push(user);
+        }
+        else {
+            inValidUsers.push(user);
+        }
+    });
+    return {
+        validUsers,
+        inValidUsers
+    }
 }
 
 
-function IsValid(user,users) {
-  return (!duplicateId(users,user.id) && isValidEmail(user.email)
-   && isValidAge(user.age));
+function IsValid(user, users) {
+    return (!duplicateId(users, user.id) && isValidEmail(user.email)
+        && isValidAge(user.age));
 }
 
 
 function isValidEmail(email) {
-  return !!email && strConatin(email, '@');
+    return !!email && strConatin(email, '@');
 
 }
 function strConatin(str, sub) {
-  return str.indexOf(sub) > -1
+    return str.indexOf(sub) > -1
 
 }
 function isValidAge(age) {
-  return age > 0;
+    return age > 0;
 }
 
 function duplicateId(users, id) {
 
-  let count = 0;
-  let dup = false;
-  users.forEach(user => {
-      if (user.id == id) {
-          count++;
-      }
-  });
-  if (count > 1) {
-      // console.log('duplicate dtected');
-      dup = true;
-  }
-  return dup;
+    let count = 0;
+    let dup = false;
+    users.forEach(user => {
+        if (user.id == id) {
+            count++;
+        }
+    });
+    if (count > 1) {
+        // console.log('duplicate dtected');
+        dup = true;
+    }
+    return dup;
 }
-
 function printOfValidUsers(user) {
-  console.log(user);
+    console.log(user);
 }
 function writeUsersToFile(users) {
-  let dataainjson = JSON.stringify(users);
-  fs.writeFileSync("./users.json", dataainjson);
+    let datainjson = JSON.stringify(users);
+    fs.writeFileSync("./users.json", datainjson);
+}
+function printValidUsersToFile(ValidUsers){
+    let Valid = JSON.stringify(ValidUsers);
+    fs.writeFileSync("./validusers.json", Valid);  
 }
